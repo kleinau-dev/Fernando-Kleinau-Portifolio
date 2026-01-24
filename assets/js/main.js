d374620ba37e8dab2ab4c774084c127f361c0713
Fernando Kleinau <fernandokleinau@gmail.com>
2026-01-24 06:53:32 -0300

    Atualização de visual, fonte e cusor

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,22 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+});
+
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    cursor.style.transform = 'translate(-50%, -50%) scale(1.5)';
+  });
+
+  link.addEventListener('mouseleave', () => {
+    cursor.style.transform = 'translate(-50%, -50%) scale(1)';
+  });
+});
